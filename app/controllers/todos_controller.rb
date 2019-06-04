021d592a45e6e7cc591e12f2ab032f8611c2f9d3
alex <alcebaeza@gmail.com>
2019-06-04 11:17:42 -0400

    listo index, new y create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,23 @@
 class TodosController < ApplicationController
     def index 
         @todos = Todo.all 
+
         
+    end
+
+    def new 
+        @todo = Todo.new 
     end
     def create 
         @todo = Todo.new(todo_params)
         @todo.completed = false 
         @todo.save 
         redirect_to todos_path
+
+end
+
+private 
+def todo_params
+    params.require(:todo).permit(:description, :completed)
+end
 end
