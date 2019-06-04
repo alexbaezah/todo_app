520b3ee50bd5e0dab09aac34153d946d7b86ec59
alex <alcebaeza@gmail.com>
2019-06-04 14:09:24 -0400

    ToDo app lista

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,42 +1,62 @@
 class TodosController < ApplicationController
-    def index 
-        @todos = Todo.all 
 
-        
+    def index
+      @todos = Todo.all.order(:completed)
     end
-
-    def new 
-        @todo = Todo.new 
+  
+    def new
+      @todo = Todo.new
     end
-    def create 
-        @todo = Todo.new(todo_params)
-        @todo.completed = false 
-        @todo.save 
-        redirect_to todos_path
-
-end
-
-def show 
-    @todo = Todo.find(params[:id])
-    if @todo.completed == true 
-        @made = 'Tarea completada'
-    else 
-        @made = 'Tarea incompleta' 
+  
+    def create
+      @todo = Todo.new(todo_params)
+      @todo.completed = false
+      @todo.save 
+      redirect_to todos_path
+    
     end
-def edit 
-    @todo = Todo.find(params[:id])
-end
-def update 
-    @todo = Todo.find(params[:id])
-    @todo.update(todo_params)
-    redirect_to todos_path
-end
-
-end
-
-
-private 
-def todo_params
-    params.require(:todo).permit(:description, :completed)
-end
-end
+  
+    def show
+      @todo =  Todo.find(params[:id])
+      if @todo.completed == true
+        @made = "Está hecha."
+      else
+        @made = "Aun no ha sido completada."
+      end
+    end
+  
+    def edit
+      @todo =  Todo.find(params[:id])
+    end
+  
+    def update
+      @todo =  Todo.find(params[:id])
+      @todo.update(todo_params)
+      redirect_to todos_path
+    end
+  
+    def destroy
+      @todo =  Todo.find(params[:id])
+      @todo.delete
+      redirect_to todos_path
+    end
+  
+    def complete
+      @todo =   Todo.find(params[:id])
+      @todo.update(completed: true)
+      redirect_to todos_path
+    end
+  
+    def list
+      @todos_unmade = Todo.all.where(completed: false)
+      @todos_made = Todo.all.where(completed: true)
+    end
+  
+ 
+  private
+  def todo_params
+      params.require(:todo).permit(:description, :completed)
+    end
+  
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new'
   post 'todos', to: 'todos#create'
-  get  'todos/:id', to: 'todos#show', as: 'todo'
+  get 'todos/list'
+  get 'todos/:id', to: 'todos#show', as: 'todo'
   get 'todos/:id/edit', to: 'todos#edit', as: 'todo_edit'
   patch 'todos/:id', to: 'todos#update'
+  delete 'todos/:id', to: 'todos#destroy'
+  get 'todos/:id/complete', to: 'todos#complete', as: 'todo_completed'
+  root 'todos#index'
+
 end
